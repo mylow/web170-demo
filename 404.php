e5f9710883f6588aaa583a8306d55a905c67a0a0
milo <me@milo.sh>
2016-04-19 13:45:34 -0700

    Revert "a_1"
    This reverts commit 8524c15894b40814063cad6de481f361071b63a6.

--- 404.php
+++
@@ -1,5 +0,0 @@
-<?php include 'includes/header.php';?>
-<?php include 'includes/nav.php';?>
-<div id="content">
-        <h1 style="margin-bottom:80%">Uh-oh, something went wrong. This is a 404 error page.</h1>
-<?php include 'includes/footer.php';?>
